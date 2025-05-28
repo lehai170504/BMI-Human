8a8d861262301dbbf4b96934e5098737fdc8a21a
lehai170504 <hoanghaile175@gmail.com>
2025-05-28 11:01:54 +0700

    Fix

--- bmi-backend/src/routes/index.ts
+++ bmi-backend/src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth';
-import bmiRoutes from './bmi';
-import userRoutes from './user';
+// import bmiRoutes from './bmi';
+// import userRoutes from './user';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/bmi', bmiRoutes);
-router.use('/user', userRoutes);
+// router.use('/bmi', bmiRoutes);
+// router.use('/user', userRoutes);
 
 export default router; 
